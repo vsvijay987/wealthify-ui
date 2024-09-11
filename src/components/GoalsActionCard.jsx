95b5b1bc34e6e87747f4db1b50b132037f1dbdcf
Vijay Sahu <vijaysahu@Vijays-MacBook-Air.local>
2024-09-11 15:17:37 +0530

    goals card

--- src/components/GoalsActionCard.jsx
+++ src/components/GoalsActionCard.jsx
@@ -1,6 +1,15 @@
 const GoalsActionCard = () => {
   return (
-    <div className="h-full w-full flex justify-center items-center"> Goals</div>
+    <div className="h-full w-full flex justify-center items-center">
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+        <ul className="space-y-3">
+          <li className="flex justify-between">
+            <span className="font-medium text-primary">Goals:</span>
+            <span className="text-textSecondary">Trip to Goa</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
