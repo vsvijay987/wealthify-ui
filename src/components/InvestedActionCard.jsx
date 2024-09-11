0a2ac726a7363a3ad10608a1323a95b5b81e3577
Vijay Sahu <vijaysahu@Vijays-MacBook-Air.local>
2024-09-11 13:59:54 +0530

    remove images

--- src/components/InvestedActionCard.jsx
+++ src/components/InvestedActionCard.jsx
@@ -4,7 +4,7 @@ const InvestedActionCard = () => {
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
         <ul className="space-y-3">
           <li className="flex justify-between">
-            <span className="font-medium text-primary">Invest Amount:</span>
+            <span className="font-medium text-primary">Invested Amount:</span>
             <span className="text-textSecondary">1000</span>
           </li>
           <li className="flex justify-between">
